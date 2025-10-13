1150e5ac0a9a720a5e3b647cdcc6eec3e2c9f397
nagendrakatigowri <nagendrakatigowri143@gmail.com>
2025-10-13 13:19:27 -0400

    changes for bot

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleChat() {
   const chatBox = document.getElementById("chat-box");
   chatBox.classList.toggle("hidden");
 }
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show');
+}
